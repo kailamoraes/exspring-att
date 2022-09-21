1841b441b57bee74af6df092584040894ecc33cf
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-09-21 17:56:44 +0000

    21-09

--- app/src/main/java/exspring/controllers/GeneroController.java
+++ app/src/main/java/exspring/controllers/GeneroController.java
@@ -1,6 +1,6 @@
 package exspring.controllers;
 
-import org,springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +13,16 @@ public class GeneroController {
     @Autowired
     private GeneroRepository generosRepo;
 
+//Model é uma classe, na qual é utilizada quando se quer passar informações à camada do usuário (JSP)
+// model.addAttribute Cria uma variavel chamada gênero
     @RequestMapping("list")
     public String list(Model model) {
         model.addAttribute("generos", this.generosRepo.findAll());
         return "list";
+    }
+
+    @RequestMapping("insert")
+    public String insert() {
+        return "insert";
     }
 }
